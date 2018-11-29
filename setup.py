81ca59c090c2c7f23ea2ed8bfe7496a33810bdcb
Richard Li <foxerlee1@gmail.com>
2018-11-29 10:12:16 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(name='kesmarag-swpt',
       author_email='kesmarag@gmail.com',
       url='https://github.com/kesmarag/stationary-wavelet-packet-transform',
       packages=['kesmarag.swpt'],
-      #package_dir={'kesmarag.swpt': './'},
+      package_dir={'kesmarag.swpt': '.'},
       install_requires=['PyWavelets>=0.5.2'], )
