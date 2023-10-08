1924db71ba5c01d1c5af10b396b01707e6422969
Costas Smaragdakis <kesmarag@gmail.com>
2023-10-08 09:29:03 +0300

    update

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='kesmarag-swpt',
-      version='0.1.4',
+      version='0.2.0',
       description='Stationary Wavelet Packet Transform',
       author='Costas Smaragdakis',
       author_email='kesmarag@gmail.com',
